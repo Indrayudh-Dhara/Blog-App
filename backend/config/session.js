12195e34a6d43d345b1668489075f1d16373e3e5
Indrayudh Dhara <dharaindrayudh16@gmail.com>
2025-05-18 04:08:22 +0530

    added changes

--- backend/config/session.js
+++ backend/config/session.js
@@ -19,10 +19,6 @@ const sessionConfig = {
     expires: Date.now()+ 7*24*60*60*1000, // 1 week
     maxAge :  7*24*60*60*1000,
     httpOnly: true,
-    
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-    secure: process.env.NODE_ENV === 'production',
-    
   }
 };
 
